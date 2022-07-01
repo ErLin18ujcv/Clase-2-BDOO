f889f607870f7d45eed415960e61d055dcaa1a02
erlin <erlinmejia98.emma@gmail.com>
2022-06-30 22:43:45 -0600

    Initial commit

---
+++ Clase#2-BDOO.py
@@ -0,0 +1,24 @@
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    '''
+    def area_cuadrado(lado):
+        return  lado * lado
+    def area_circulo():
+        pass
+    cuadrado1= area_cuadrado(5)
+    print(cuadrado1)
+'''
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
